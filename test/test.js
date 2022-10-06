c1664c8177e0141f3422eebbc6ca070767320503
Chong Wah Ng <CNg@ppg.com>
2022-10-06 06:24:45 +0000

    add more odata test

--- test/test.js
+++ test/test.js
@@ -18,7 +18,7 @@ else cds.User = cds.User.Privileged // hard core monkey patch for older cds rele
 describe("running simple unit test", () => {
     before(() => { })
 
-    it('Allows testing programmatic APIs', async () => {
+    it('[Test]: service APIs', async () => {
         const UserService = await cds.connect.to('UserService')
         const { Users } = UserService.entities
         expect(await SELECT.from(Users))
@@ -26,11 +26,18 @@ describe("running simple unit test", () => {
             .to.eql(await UserService.run(SELECT.from(Users)))
     })
 
-    it("serves custom endpoint", async () => {
-        const {
-            headers,
-            status
-        } = await GET("/user/$metadata", {});
+    it("[Test]: serves $metadata documents in v4", async () => {
+        const { headers, status, data } = await GET("/user/$metadata");
         expect(status).to.equal(200);
+        expect(headers).to.contain({
+            'content-type': 'application/xml',
+            'odata-version': '4.0',
+        })
+        expect(data).to.contain('EntitySet Name="Users" EntityType="UserService.Users"')
+    })
+
+    it('[Test]: serves /user/Users', async () => {
+        const { data } = await GET("/user/Users")
+        expect(data.value).to.eql([{ ID: 'MYID001', name: 'cds.test() User' }])
     })
 })
