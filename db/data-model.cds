0cd168e9e18a51adc8a31db2fc8c21193d03e7c0
Chong Wah Ng <CNg@ppg.com>
2022-10-12 03:00:16 +0000

    consolidated changes ww3-7oct

--- db/data-model.cds
+++ db/data-model.cds
@@ -1,7 +1,9 @@
 namespace db;
 
 entity Users {
-    key ID   : String(10);
-        name : String(100);
+    key ID          : String(10);
+        name        : String(100);
+        dateOfBirth : Date;
+        email       : String(100);
+        isLock      : Boolean;
 }
-
--- test/test.js
+++ test/test.js
@@ -2,7 +2,14 @@ const cds = require("@sap/cds/lib");
 const { expect, GET, POST, PATCH, DEL, data } = cds.test("serve", "--in-memory", "--project", `${__dirname}/..`)
 
 describe("running simple unit test", () => {
-    before(() => { })
+    beforeAll(async () => {
+        const db = await cds.connect.to('db')
+        const { Users } = db.model.entities('db')
+        await db.create(Users).entries([
+            { ID: 'USR01', name: 'user 1 created in cds.test', dateOfBirth: '1990-01-20', email: 'usr01@gmail.com', isLock: false },
+            { ID: 'USR02', name: 'user 2 created in cds.test', dateOfBirth: '1992-02-20', email: 'usr02@gmail.com', isLock: false }
+        ])
+    })
 
     it('[Test]: service APIs', async () => {
         const UserService = await cds.connect.to('UserService')
@@ -13,7 +20,7 @@ describe("running simple unit test", () => {
     })
 
     it("[Test]: serves $metadata documents in v4", async () => {
-        const { headers, status, data } = await GET("/user/$metadata");
+        const { headers, status, data } = await GET(`/user/$metadata`);
         expect(status).to.equal(200);
         expect(headers).to.contain({
             'content-type': 'application/xml',
@@ -23,7 +30,20 @@ describe("running simple unit test", () => {
     })
 
     it('[Test]: serves /user/Users', async () => {
-        const { data } = await GET("/user/Users")
-        expect(data.value).to.eql([{ ID: 'MYID001', name: 'cds.test() User' }])
+        const { data } = await GET(`/user/Users`)
+        expect(data.value).to.eql([
+            { ID: 'USR01', name: 'user 1 created in cds.test', dateOfBirth: '1990-01-20', email: 'usr01@gmail.com', isLock: false },
+            { ID: 'USR02', name: 'user 2 created in cds.test', dateOfBirth: '1992-02-20', email: 'usr02@gmail.com', isLock: false }
+        ])
+    })
+
+    it('[Test]: supports $select', async () => {
+        const { data } = await GET(`/user/Users`, {
+            params: { $select: `ID, name, email` }
+        })
+        expect(data.value).to.eql([
+            { ID: 'USR01', name: 'user 1 created in cds.test', email: 'usr01@gmail.com' },
+            { ID: 'USR02', name: 'user 2 created in cds.test', email: 'usr02@gmail.com' }
+        ])
     })
 })
