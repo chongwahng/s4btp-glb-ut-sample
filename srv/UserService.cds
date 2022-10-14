3be72a28dde7ed4e1efa54fb45576347784ec360
Chong Wah Ng <CNg@ppg.com>
2022-10-14 09:34:46 +0000

    add instance based authorization

--- srv/UserService.cds
+++ srv/UserService.cds
@@ -1,13 +1,22 @@
 using {db} from '../db/data-model';
 
 service UserService @(path : '/user') {
+    @readonly
     entity Users as projection on db.Users;
+
     action lockAccount(id : Users:ID);
     action unlockAccount(id : Users:ID);
 }
 
+service APIService @(
+    requires : 'authenticated-user',
+    path     : '/api'
+) {
+    function userInfo() returns String;
+}
+
 annotate UserService.Users with @(restrict : [{
-    grant : ['READ'],
+    grant : '*',
     to    : 'Admin',
-    where : 'department = $user.department'
+    where : '$user.Department = department'
 }]);
--- srv/UserService.js
+++ srv/UserService.js
@@ -1,3 +1,5 @@
+const { retrieveJwt, decodeJwtComplete } = require("@sap-cloud-sdk/core")
+
 module.exports = async function (srv) {
     const cds = require('@sap/cds')
 
@@ -11,5 +13,22 @@ module.exports = async function (srv) {
         const { Users } = cds.entities
         let query = UPDATE(Users).set({ isLock: false }).where({ ID: req.data.id })
         await cds.run(query)
+    })
+
+    srv.on("userInfo", async (req) => {
+        var token = retrieveJwt(req)
+
+        console.log(`User data -> ${JSON.stringify(req.user)}`)
+        console.log(`User ID -> ${req.user.id}`)
+        console.log(`User attr -> ${JSON.stringify(req.user.attr)}`)
+        console.log(`Is user authenticated? ${req.user.is('authenticated-user')}`)
+        console.log(`Is system-user? ${req.user.is('system-user')}`)
+        console.log(`Is internal-user? ${req.user.is('internal-user')}`)
+
+        if (token) {
+            var decodedJWT = decodeJwtComplete(token)
+            console.log(decodedJWT)
+            return JSON.stringify(decodedJWT)
+        }
     })
 }
